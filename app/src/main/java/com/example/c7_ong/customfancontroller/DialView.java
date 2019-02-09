aa9cb3488ae2528896ff93f64b27d7acba97bc3f
changchen96 <twt6m@hotmail.com>
2019-02-09 20:23:41 +0000

    Completed Coding Challenge 1

--- app/src/main/java/com/example/c7_ong/customfancontroller/DialView.java
+++ app/src/main/java/com/example/c7_ong/customfancontroller/DialView.java
@@ -1,6 +1,7 @@
 package com.example.c7_ong.customfancontroller;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
@@ -9,10 +10,6 @@ import android.util.AttributeSet;
 import android.view.View;
 
 public class DialView extends View{
-    public DialView(Context context) {
-        super(context);
-        init();
-    }
     private static int SELECTION_COUNT = 4;
     private float mWidth;
     private float mHeight;
@@ -20,22 +17,76 @@ public class DialView extends View{
     private Paint mDialPaint;
     private float mRadius;
     private int mActiveSelection;
+    private int mFanOnColor;
+    private int mFanOffColor;
     //string buffer for dial labels, float for computeXY result
     private final StringBuffer mTempLabel = new StringBuffer(0);
     private final float[] mTempResult = new float[2];
 
-    private void init()
+    //standard constructor for the class
+    public DialView(Context context) {
+        super(context);
+        init(context, null);
+    }
+
+    //constructor is called when a view is built from an XML file
+    public DialView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    //constructor is called to supply the default style
+    public DialView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    //this function is used to initialize instance variables, it is called by constructors
+    private void init(Context context, AttributeSet attrs)
     {
+
         mTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mTextPaint.setColor(Color.BLACK);
         mTextPaint.setStyle(Paint.Style.FILL_AND_STROKE);
         mTextPaint.setTextSize(40f);
         mDialPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mDialPaint.setColor(Color.GRAY);
+        mFanOffColor = Color.CYAN;
+        mFanOnColor = Color.GRAY;
+        mDialPaint.setColor(mFanOffColor);
         //Initialize current selection
         mActiveSelection = 0;
+
+        if (attrs != null)
+        {
+            TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.DialView, 0,0);
+            mFanOnColor = typedArray.getColor(R.styleable.DialView_fanOnColor, mFanOnColor);
+            mFanOffColor = typedArray.getColor(R.styleable.DialView_fanOffColor, mFanOffColor);
+            typedArray.recycle();
+        }
+
+        setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mActiveSelection = (mActiveSelection + 1) % SELECTION_COUNT;
+                if (mActiveSelection >= 1)
+                {
+                    mDialPaint.setColor(mFanOnColor);
+                }
+                else
+                {
+                    mDialPaint.setColor(mFanOffColor);
+                }
+                invalidate();
+            }
+
+        });
     }
 
+    //called during layout when the size of the view has changed
+    //w = current width
+    //h = current height
+    //oldw = old width
+    //oldh = old height
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh)
     {
@@ -44,6 +95,7 @@ public class DialView extends View{
         mRadius = (float) (Math.min(mWidth, mHeight)/2*0.8);
     }
 
+    //gets the X/Y coordinates for a label or indicator
     private float[] computeXYForPosition(final int pos, final float radius)
     {
         float[] result = mTempResult;
@@ -54,6 +106,7 @@ public class DialView extends View{
         return result;
     }
 
+    //render the view content
     @Override
     protected void onDraw (Canvas canvas)
     {
@@ -78,13 +131,4 @@ public class DialView extends View{
         canvas.drawCircle(x,y,20,mTextPaint);
     }
 
-    public DialView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-        init();
-    }
-
-    public DialView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        init();
-    }
 }
